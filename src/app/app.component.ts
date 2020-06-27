bd7c6d12a5bb8b1d1a9aa37f6991b9856891116e
DerbelSahar <derbel.sahar@insat.u-carthage.tn>
2020-06-27 21:52:23 +0100

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'authentificationClient';
+  private roles: string[];
+  isLoggedIn:boolean ;
+  showAdminBoard:boolean ;
+  showModeratorBoard:boolean;
+  username: string;
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = false;
+    this.showAdminBoard = false;
+    this.showModeratorBoard = false;
+    this.username = '';
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout() {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
